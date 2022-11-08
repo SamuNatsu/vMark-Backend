bc06ad9d04c02c3fcc381552808daab821fe2190
SamuNatsu <samunatsu@qq.com>
2022-11-08 20:09:53 +0800

    [2022/11/08 20:07] Initialize

---
+++ src/main/java/com/vmark/backend/VMarkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vmark.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VMarkBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VMarkBackendApplication.class, args);
+    }
+
+}
